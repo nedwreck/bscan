a0030facdc558592a5bfa143d8635a4dac47edba
nedwreck <josheddy@protonmail.com>
2021-01-04 21:19:00 -0700

    second

---
+++ bash/node/node0getnewdomains.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cat ~/nodemount/data/massdns/newfreshaa > ~/newfresh.txt
+cat ~/nodemount/data/massdns/freshliveaa > ~/freshlive.txt
---
+++ bash/node/nodesrunfreshdomains.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Get list of domains for this node to resolve
+
+cd ~/ &&\
+
+./newfresh.sh &&\
+
+# Get resolvers.txt for massdns
+sort -R ~/bscan/data/dnsresolver/bigresolvers2.txt | tail -n 200 > massdnsresolvers.txt &&\
+
+# Running massdns on fresh domains
+# Strip massdns output of record type and IP and feed that into dnsgen
+# Feed dnsgen output directly into massdns again
+./massdns/bin/massdns -r massdnsresolvers.txt -o S newfresh.txt \
+| sed 's/\s.*$//'                                               \
+| dnsgen -                                                      \
+| ./massdns/bin/massdns \
+-r massdnsresolvers.txt \
+-w newfreshlivedomains.txt \
+-o S  &&\
+
+# Sort massdns output
+sort newfreshlivedomains.txt | uniq > newfreshlivedomains2.txt &&\
+
+# Add this node's newfresh.txt to alreadyrundomains.txt
+cat newfresh.txt >> ~/bscan/data/massdns/alreadyrundomains.txt &&\
+
+# Add the massdnsoutput to freshlivedomains.txt
+# Currently only looking for cname records to avoid wildcard domain crap
+cat newfreshlivedomains2.txt | grep "CNAME" | cat > freshlivecname.txt &&\
+
+cat freshlivecname.txt >> ~/bscan/data/massdns/freshlivedomains.txt
