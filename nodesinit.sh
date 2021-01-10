97475a5e0e81d62ed399d9aba8c0b57b2c5b67e4
nedwreck <josheddy@protonmail.com>
2021-01-10 11:17:56 -0700

    moar

--- nodesinit.sh
+++ nodesinit.sh
@@ -5,7 +5,19 @@ ansible-playbook node/nodeinit.yml
 
 # Have each node copy it's particular script over for getting a newfreshdomains split
 ansible-playbook node/0/getscripts.yml &&\
-#ansible-playbook node/1/getscripts.yml &&\
-#ansible-playbook node/2/getscripts.yml &&\
-#ansible-playbook node/3/getscripts.yml &&\
-#ansible-playbook node/4/getscripts.yml
+ansible-playbook node/1/getscripts.yml &&\
+ansible-playbook node/2/getscripts.yml &&\
+ansible-playbook node/3/getscripts.yml &&\
+ansible-playbook node/4/getscripts.yml &&\
+ansible-playbook node/5/getscripts.yml &&\
+ansible-playbook node/6/getscripts.yml &&\
+ansible-playbook node/7/getscripts.yml &&\
+ansible-playbook node/8/getscripts.yml &&\
+ansible-playbook node/9/getscripts.yml &&\
+ansible-playbook node/10/getscripts.yml &&\
+ansible-playbook node/11/getscripts.yml &&\
+ansible-playbook node/12/getscripts.yml &&\
+ansible-playbook node/13/getscripts.yml &&\
+ansible-playbook node/14/getscripts.yml &&\
+ansible-playbook node/15/getscripts.yml &&\
+
